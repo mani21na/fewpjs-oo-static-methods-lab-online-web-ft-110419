f8a3faf44461eaeec136e5c52d73bda840b8b0ea
Hanna Lee <mani21na@gmail.com>
2020-03-15 05:47:31 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -8,21 +8,20 @@ class Formatter {
   }
 
   static titleize( sentence ) {
-  let exceptions = [ 'the', 'a', 'an', 'but', 'of', 'and', 'for', 'at', 'by', 'from' ]
-  let result = [];
-  let arrayOfWords = sentence.split( " " )
-  for ( let n = 0; n < arrayOfWords.length; n++ ) {
-    if ( n == 0 ) {
-      result.push( this.capitalize( arrayOfWords[ n ] ) )
-    } else {
-      if ( exceptions.includes( arrayOfWords[ n ] ) ) {
-        result.push( arrayOfWords[ n ] )
+    let exceptions = [ 'the', 'a', 'an', 'but', 'of', 'and', 'for', 'at', 'by', 'from' ]
+    let result = [];
+    let arrayOfWords = sentence.split( " " )
+    for ( let n = 0; n < arrayOfWords.length; n++ ) {
+      if ( n == 0 ) {
+        result.push( this.capitalize( arrayOfWords[ n ] ) ) 
       } else {
-        result.push( this.capitalize( arrayOfWords[ n ] ) )
+        if ( exceptions.includes( arrayOfWords[ n ] ) ) {
+          result.push( arrayOfWords[ n ] )
+        } else {
+          result.push( this.capitalize( arrayOfWords[ n ] ) )
+        }
       }
     }
-
+    return result.join( " " );
   }
-  return result.join( " " );
-}
 }
