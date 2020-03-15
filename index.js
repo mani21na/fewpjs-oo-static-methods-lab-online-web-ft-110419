4b970edae13909a426e7c7a80a33d94b463baa84
Hanna Lee <mani21na@gmail.com>
2020-03-15 05:45:56 +0000

    Done.

--- index.js
+++ index.js
@@ -1,3 +1,9 @@
 class Formatter {
-  //add static methods here
+  static capitalize(string) {
+    return string.charAt(0).toUpperCase() + string.slice(1);
+  }
+
+  static sanitize(string) {
+    return string.replace(/[^\w,'-]/, '');
+  }
 }
